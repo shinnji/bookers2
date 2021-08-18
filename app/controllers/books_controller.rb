95c317ab78e362d3c09465fe462a30dc2b085200
shinnji <ec2-user@ip-172-31-21-242.ec2.internal>
2021-08-18 09:22:19 +0000

    header-font-create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,21 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   def destroy
-  end 
-  
+  end
+
   def edit
-  end 
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 root to: 'homes#top'
 resources :books
 resources :users
+get "home/about" => "homes#about" 
 end
 
 
