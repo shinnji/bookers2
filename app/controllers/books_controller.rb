675d2d20c0f885fbc73424885c37d30c24f05b39
shinnji <ec2-user@ip-172-31-21-242.ec2.internal>
2021-08-19 12:34:37 +0000

    user-show-path-finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,32 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+     @book.user_id = current_user.id
+     @book.save
+     redirect_to book_path(@book)
   end
 
   def index
+    @books =Book.all
   end
 
   def show
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
   end
 
   def edit
+  end
+
+    private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
+
+    def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
+    end
+
+    def create
+
+    end
+
+    def new
+      @book = Book.new
+    end
 end
